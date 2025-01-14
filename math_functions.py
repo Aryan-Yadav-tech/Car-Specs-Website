98906e10c4eb50e683ebf42bf80bc8fc72ae8c2e
Aryan Yadav <ay062265@gmail.com>
2025-01-14 21:37:39 +0530

    Add files via upload

---
+++ math_functions.py
@@ -0,0 +1,17 @@
+#math functions
+
+import math
+a=-51
+print(a)
+print(abs(a))
+print(math.sin(1))
+print(math.cos(2))
+print(math.ceil(2.3))
+print(math.radians(5.6))
+print(math.floor(2.9))
+print(math.sqrt(25))
+print(math.cbrt(125))
+print(math.pi)
+print(math.e)
+print(min(12,23,54,23,65))
+print(math.factorial(5))
